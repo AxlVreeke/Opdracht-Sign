f229a007bf811a96e1d02720323818bf5b402e6f
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-03-14 10:48:57 +0100

    User database migration aangemaakt.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phone_number')->unique();
+            $table->string('street_name');
+            $table->integer('house_number');
+            $table->string('postal_code');
+            $table->string('residence');
+            $table->string('region');
+            $table->string('country');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
