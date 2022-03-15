02b718709f625a62b5fa0b60a085b5170ac8c435
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-03-15 09:33:04 +0100

    Opdrachten database en model aangemaakt.

--- database/migrations/2022_03_15_081807_create_jobs_table.php
+++ database/migrations/2022_03_15_081807_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
