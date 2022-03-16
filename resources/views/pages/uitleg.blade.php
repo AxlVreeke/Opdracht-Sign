d5f89a910ce37948efead607fb0bdbc553301d92
AxlVreeke <D283491@edu.rocwb.nl>
2022-03-16 09:40:39 +0100

    updated uitleg pagina

--- resources/views/pages/uitleg.blade.php
+++ resources/views/pages/uitleg.blade.php
@@ -1,5 +1,43 @@
 @extends('base')
 
 @section('content')
-    <h1>Uitleg</h1>
+    <div class="container">
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><span>100% </span>beveelt Sign aan!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">9.5 van de 10 Sterren</h6>
+                    <p class="card-text">Sign is oprecht een banger</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><span>100% </span>beveelt Sign aan!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">9 van de 10 Sterren</h6>
+                    <p class="card-text">Sign gets all</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><span>100% </span>beveelt Sign aan!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">7.5 van de 10 Sterren</h6>
+                    <p class="card-text">cool</p>
+                </div>
+            </div>
+            <ul class="uitleg">
+                <li>UITLEG</li>
+            </ul>
+            <div class="explanation-content">
+                <h2>Start uw eigen schoolproject</h2>
+                <h3>Het duurt slechts enkele minuten!</h3>
+                <div class="explanation-images">
+                    <img src="img/school.jpg" alt="schoolfoto 1">
+                    <img src="img/school2.jpg" alt="schoolfoto 2">
+                    <img src="img/school3.jpg" alt="schoolfoto 3">
+                    <p>cool</p>
+                </div>
+                <p>test</p>
+            </div>
+        </div>
+    </div>
 @endsection
