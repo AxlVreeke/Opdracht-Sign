a2e62c4c4d80110d61331655d0b68c35669ec9bc
AxlVreeke <D283491@edu.rocwb.nl>
2022-03-17 09:41:03 +0100

    carousel is now working

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,40 +1,40 @@
 @extends('base')
 
 @section('content')
-    <style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    </style>
-
-    <div id="#myCarousel" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="overlay-image" style="background-image:url(../img/placeholder-home.avif);"></div>
-                <div class="carousel-content">
-                    <h1>Welkom op de Sign page!</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid architecto aut cupiditate deleniti, error ex exercitationem harum hic maxime, necessitatibus nobis quos recusandae sequi, tenetur vel voluptas voluptatem voluptates.</p>
+                <img class="d-block w-100" src="https://placeimg.com/1080/500/animals" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Welkom op de Sign page!</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda blanditiis corporis doloremque, ea nobis porro repellat reprehenderit. Beatae consectetur nam nisi porro? Accusamus aliquid itaque laborum nobis nulla pariatur?</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="overlay-image" style="background-image:url(../img/placeholder-home.avif);"></div>
-                <div class="carousel-content">
-                    <h1>Example Headline</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid architecto aut cupiditate deleniti, error ex exercitationem harum hic maxime, necessitatibus nobis quos recusandae sequi, tenetur vel voluptas voluptatem voluptates.</p>
-                </div>
+                <img class="d-block w-100" src="https://placeimg.com/1080/500/arch" alt="Second slide">
             </div>
-            <a href="#myCarousel" class="carousel-control-prev" role="button" data-slide="prev">
-
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a href="#myCarousel" class="carousel-control-next" role="button" data-slide="next">
-                <span class="sr-only">Next</span>
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            </a>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://placeimg.com/1080/500/nature" alt="Third slide">
+            </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
     <div class="container">
         <div class="flex-wrapper">
