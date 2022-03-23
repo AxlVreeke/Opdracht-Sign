36821a5d357710f865d130a86efeca3313c519d3
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-03-23 10:16:58 +0100

    Update index.blade.php

--- resources/views/dashboard/exercises/index.blade.php
+++ resources/views/dashboard/exercises/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <div class="container">
-    <h1>Opdrachten</h1>
+    <ul class="ul-title">
+        <li>OPDRACHTEN</li>
+    </ul>
     <div class="input-group mb-3">
     <label class="input-group-text" for="inputGroupSelect01">Categorie:</label>
     <select class="form-select" id="inputGroupSelect01">
@@ -21,20 +23,24 @@
 
     <a href="{{route('exercises.create')}}" class="btn btn-primary">Nieuw opdracht aanmaken</a>
 
-    <div class="container">
-        <table class="table">
-            <thead>
+
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Naam <></th>
+            <th scope="col">Klus <></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($exercises as $exercise)
             <tr>
-                <th>Name</th>
-                <th>Klus</th>
+                <td>{{ $exercise->id }}</td>
+                <td>{{ $exercise->name }}</td>
+                <td>{{ $exercise->job }}</td>
             </tr>
-            </thead>
-            @foreach($exercises as $exercise)
-                <tr>
-                    <td class="table-dark">{{ $exercise->name }}</a></td>
-                    <td class="table-dark">{{ $exercise->job }}</td>
-                </tr>
-            @endforeach
-        </table>
+        @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
