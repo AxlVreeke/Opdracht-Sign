2485c77f1e258ddf33905ef02405767e70d08a5e
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2022-04-19 11:51:05 +0200

    Participate database table aangemaakt.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -48,9 +48,9 @@
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
                                     <a class="dropdown-item" href="{{ route('exercise.index') }}">Profiel</a>
-
+                                    @role('admin')
                                     <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
-
+                                    @endrole
                                     <a class="dropdown-item" href="{{ route('logout') }} "
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
